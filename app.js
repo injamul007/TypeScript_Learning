4d5d991a54c031d3be6d0df4f1320232eb541606
injamul007 <injamul007hoque@gmail.com>
2026-02-04 11:14:22 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+"use strict";
+//? Basic Types
+//? •Primitive types (number, string, boolean)
+//? •Arrays
+//? •Tuples (Fixed Value of an array in place)
+//? •Enums
+//? •Any, Unknown, Void, Null, Undefined, Never
+Object.defineProperty(exports, "__esModule", { value: true });
+// var a = [1,2,3,4,5]
+// var b = [...a]
+// b.pop()
+// console.log(b)
+// console.log(a)
+// let c = 12
+// c = 'boss'
+// let arr: number[] = [1,2,3]
+//? Tuples
+let arr = ["boss", 22];
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,77 @@
+//? Basic Types
+//? •Primitive types (number, string, boolean)
+//? •Arrays
+//? •Tuples (Fixed Value of an array in place)
+//? •Enums
+//? •Any, Unknown, Void, Null, Undefined, Never
+
+// var a = [1,2,3,4,5]
+// var b = [...a]
+// b.pop()
+// console.log(b)
+// console.log(a)
+// let c = 12
+// c = 'boss'
+
+// let arr: number[] = [1,2,3]
+
+//? Tuples
+// let arr: [string, number] = ["boss", 22]
+
+//? Enum / Enumerations
+// enum userRoles {
+//   Admin = "admin",
+//   Guest = "guest",
+//   Super_Admin = "super_admin"
+// }
+
+// userRoles.Admin
+
+// enum StatusCode {
+//   Abandoned = "abandoned status code 500",
+//   NotFound = "not found status code 404"
+// }
+
+// StatusCode.NotFound
+
+//? any and unknown
+// let a: unknown;
+
+// a = 22
+
+// a = "boss"
+
+// if(typeof a === "string")
+// a.toUpperCase()
+
+
+//? void in function
+
+// function abcd(): void {
+//   console.log("object");
+// }
+
+//? Union
+// let a: string | number
+
+// a = "boss"
+// a = null
+// a = 12
+// a = true
+
+//? Never 
+
+// function abcd(): never {
+//   while(true)
+// }
+
+// abcd()
+// console.log("object")
+
+
+//? Type Inference
+//? Understanding type inference
+//? Type annotations
+
+let a: number | string = 12
+a = "boss"
